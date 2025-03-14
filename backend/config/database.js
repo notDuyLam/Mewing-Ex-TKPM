ee494b435a0de06626d5fc111b37abc69191e1ea
MrPen <lan474444@gmail.com>
2025-03-14 23:06:04 +0700

    Add .env.example & .env.DB_URL

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-const sequelize = new Sequelize("postgres", process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "localhost",
+const sequelize = new Sequelize("TKPM", process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_URL,
     dialect: "postgres",
     logging: false
 });
