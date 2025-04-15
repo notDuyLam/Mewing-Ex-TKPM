72cf90bf53f05b16e1ed82ee3be6bee07f3ccb3c
notDuyLam <22120181@student.hcmus.edu.vn>
2025-04-15 15:22:44 +0700

    [BE]: Fix conflict seeding and migration script

--- backend/migrations/20250414172514-create-enrollment.js
+++ backend/migrations/20250414172514-create-enrollment.js
@@ -22,7 +22,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
-      staffId: {
+      registerBy: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
