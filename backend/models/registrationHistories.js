9b4b4c492f980d1ade1923c19409677ab55833a2
notDuyLam <22120181@student.hcmus.edu.vn>
2025-04-16 22:44:45 +0700

    [BE]: Add unit test and modify model

--- backend/models/registrationHistories.js
+++ backend/models/registrationHistories.js
@@ -30,6 +30,9 @@ module.exports = (sequelize, DataTypes) => {
       action: {
         type: DataTypes.ENUM("register", "cancel"),
         allowNull: false,
+        validate: {
+          isIn: [["register", "cancel"]],
+        },
       },
       performedBy: {
         type: DataTypes.INTEGER,
