cf4d4f1eaef9ff2123b23649194f684ad863609c
NgocDang <ngocdang240604@gmail.com>
2025-04-16 21:06:34 +0700

    [BE]: fix migration and seeder at RegistrationHistory

--- backend/seeders/20250415153008-seed-registration-history.js
+++ backend/seeders/20250415153008-seed-registration-history.js
@@ -6,7 +6,6 @@ module.exports = {
       {
         studentId: "SV003", // Example student ID
         classId: "C001", // Example class ID
-        credits: 3, // Credits for this course
         action: "register", // Register action
         performedBy: 1, // Staff ID who performed the action
         performAt: new Date(),
@@ -16,7 +15,6 @@ module.exports = {
       {
         studentId: "SV002", // Example student ID
         classId: "C002", // Example class ID
-        credits: 4, // Credits for this course
         action: "register", // Register action
         performedBy: 2, // Staff ID who performed the action
         performAt: new Date(),
@@ -26,7 +24,6 @@ module.exports = {
       {
         studentId: "SV001", // Example student ID
         classId: "C002", // Example class ID
-        credits: 4, // Credits for this course
         action: "cancel", // Cancel action
         performedBy: 1, // Staff ID who performed the action
         performAt: new Date(),
