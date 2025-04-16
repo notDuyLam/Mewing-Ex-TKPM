c1a5bd1791fe0f292b56b155b23cd3c179b8e357
NgocDang <ngocdang240604@gmail.com>
2025-04-16 18:18:05 +0700

    [BE]: fix classes model

--- backend/models/classes.js
+++ backend/models/classes.js
@@ -39,6 +39,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      semesterId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
     {
       sequelize,
